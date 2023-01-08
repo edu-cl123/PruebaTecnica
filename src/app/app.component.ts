053755e4671610ecace2c2c834e21aa31fb330ad
unknown <educar200@gmail.com>
2023-01-08 19:16:35 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'PruebaTrabajo';
+  test1: any[] = []
+  element = false;
+  contador = 0;
+  contador1 = 0;
+
+  displayedColumns: string[] = ['Comercio', 'Estado', 'Fecha'];
+
+  dataSource = new MatTableDataSource<any>(this.test1);
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
+  currentItem = "";
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  constructor(private papa: Papa, private _liveAnnouncer: LiveAnnouncer) {
+  }
+
+
+  showData() {
+    return (this.element = true);
+  }
+  hideData() {
+    return (this.element = false);
+  }
+  getValue(event: Event) {
+    this.dataSource.filter = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+    return (event.target as HTMLInputElement).value;
+  }
+
+  handleFileSelect(evt: any) {
+    var files = evt.target.files; // FileList object
+    this.test1 = []
+
+    for (var i = 0; i < files.length; i++) {
+
+
+      var file = files[i];
+      var reader = new FileReader();
+
+      reader.readAsText(file);
+
+      reader.onload = (event: any) => {
+        var csv = event.target.result; // Content of CSV file
+        this.papa.parse(csv, {
+          skipEmptyLines: true,
+          header: true,
+
+          complete: (results) => {
+            this.test1 = this.test1.concat(results.data)
+
+            if (i == files.length) {
+              this.contador1 = 0;
+              for (var b = 0; b < this.test1.length; b++) {
+                if (this.test1[b].Estado == "Aceptada") {
+                  this.contador1++;
+                }
+              }
+            }
+            this.dataSource.data = this.test1
+
+          }
+
+        });
+      }
+    }
+
+
+  }
+
 }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { Papa } from 'ngx-papaparse';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +26,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    FormsModule,
+    MatExpansionModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
+
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
